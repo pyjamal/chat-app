f5335f2205468f54e89ba7d07c6cb8591b70d7b6
pyjamal <limachijonas@gmail.com>
2020-10-21 12:07:55 +0200

    React 3's challenge

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import './App.css';
-import Contact from './components/Contact'
+//import Contact from './components/Contact'
+import ContactList from './components/ContactList';
+
 
 function App() {
   return (
     <div className="App">
-      <Contact name= 'Leon Davis' avatar= 'https://randomuser.me/api/portraits/men/76.jpg' online= {true}/>
-      <Contact name= 'Kathryn Riley' avatar= 'https://randomuser.me/api/portraits/women/45.jpg' online= {false}/>
-      <Contact name= 'Derek Parker' avatar= 'https://randomuser.me/api/portraits/men/75.jpg' online= {true}/>
+      <ContactList />
     </div>
   );
 }
 
 export default App;
+
+/*<Contact name= 'Leon Davis' avatar= 'https://randomuser.me/api/portraits/men/76.jpg' online= {true}/>
+      <Contact name= 'Kathryn Riley' avatar= 'https://randomuser.me/api/portraits/women/45.jpg' online= {false}/>
+      <Contact name= 'Derek Parker' avatar= 'https://randomuser.me/api/portraits/men/75.jpg' online= {true}/>
+      <ContactList />*/
