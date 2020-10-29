6ea0fbaed0877328b93802dcf690c9f400fe818a
pyjamal <limachijonas@gmail.com>
2020-10-29 17:16:14 +0100

    suppress comment

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,28 +3,6 @@ import './Contact.css';
 import PropTypes from 'prop-types';
 import { render } from '@testing-library/react';
 
-/*const Contact = (props) => (
-    <div className= 'Contact'>
-
-        <img className= 'avatar' src = {props.avatar} alt=''/>
-
-        <div>
-            <h4 className= 'name'>{props.name}</h4>
-            <div className= 'status'>
-                <div className= {props.online === true ? 'status-online' : 'status-offline'}></div>
-                <p className='status-text'>{props.online === true? 'online' : 'offline' }</p>
-            </div>
-        </div>
-           
-    </div>
-)
-
-Contact.propTypes = {
-    name: PropTypes.string.isRequired,
-    avatar: PropTypes.string.isRequired,
-    online: PropTypes.bool.isRequired
-}*/
-
 class Contact extends React.Component {
     constructor(props){
         super(props);
